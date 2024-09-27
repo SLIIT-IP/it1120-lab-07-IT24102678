845ded07a3ad7a85a93ddc197be3d6a95694d0b4
IT24102678 <it24102678@my.sliit.lk>
2024-09-27 22:28:39 +0530

    Add IT24102678LAB7Q2b.java to Q2

---
+++ Q2/IT24102678LAB7Q2b.java
@@ -0,0 +1,20 @@
+
+public class IT24102678LAB7Q2b{
+
+	public static void main(String[] args){
+	int r,c,k=1;
+	for(r=1;r<=5;r++){
+	System.out.print(r +"-");
+
+	for(c=1;c<=k;c++){
+	System.out.print("*");
+
+	}
+	System.out.println();
+	k++;
+	}
+
+
+	
+	}
+}
