bedca6c2378e2c01df48c6a31d86d78a71015215
IT24102678 <it24102678@my.sliit.lk>
2024-09-27 22:27:38 +0530

    Add IT24102678LAB7Q2A.java to Q2

---
+++ Q2/IT24102678LAB7Q2A.java
@@ -0,0 +1,18 @@
+
+
+public class IT24102678LAB7Q2A{
+
+	public static void main(String[] args){
+
+		for(int row=1;row<=4;row++){
+
+		System.out.print("$");
+		
+		for(int column=1; column<=5;column++){
+
+		System.out.print("$");
+		}
+		System.out.println();
+		}
+}
+}
